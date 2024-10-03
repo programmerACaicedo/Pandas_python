96ff99286b0b211deb77896df92c6f12fcb448d4
Alejandro Caicedo <alejandro.cajiao0525@gmail.com>
2024-10-02 21:32:36 -0500

    Abordamos el Reto de la sesión #2

--- Reto_Sesión_2/main.py
+++ Reto_Sesión_2/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-path = 'spotify_data.csv'
+path = 'Reto_Sesión_2/spotify_data.csv'
 retail_data = pd.read_csv(path, encoding = "latin1")
 print(type(retail_data))
 print(retail_data)
